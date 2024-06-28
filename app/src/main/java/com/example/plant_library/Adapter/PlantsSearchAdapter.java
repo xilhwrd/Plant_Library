b4c04128b3a2394624dc3e69bb8b1c0cdf4a3c8c
xilhwrd <teamtxpql@gmail.com>
2024-06-28 18:38:53 +0700

    detail plant

---
+++ app/src/main/java/com/example/plant_library/Adapter/PlantsSearchAdapter.java
@@ -0,0 +1,137 @@
+package com.example.plant_library.Adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.plant_library.Activity.DetailActivity;
+import com.example.plant_library.Interface.RecyclerViewInterface;
+import com.example.plant_library.Object.Plants;
+import com.example.plant_library.R;
+import com.facebook.shimmer.ShimmerFrameLayout;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class PlantsSearchAdapter extends RecyclerView.Adapter<PlantsSearchAdapter.PlantViewHolder> {
+    private final RecyclerViewInterface recyclerViewInterface;
+    private Context context;
+    private List<Plants> plantsList, plantsListFull;
+    private final int recyclerViewId;
+    private boolean showShimmer = true;
+    public PlantsSearchAdapter(List<Plants> list, RecyclerViewInterface recyclerViewInterface, Context context, int recyclerViewId) {
+        this.plantsList = list;
+        this.recyclerViewInterface = recyclerViewInterface;
+        this.context = context;
+        this.recyclerViewId = recyclerViewId;
+    }
+
+    @NonNull
+    @Override
+    public PlantViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_plants_search, parent, false);
+        return new PlantViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PlantViewHolder holder, int position) {
+        if (showShimmer) {
+            holder.shimmerLayout.startShimmer();
+            holder.shimmerLayout.setAlpha((float) 1);
+
+        } else {
+            holder.shimmerLayout.stopShimmer();
+            holder.shimmerLayout.setShimmer(null);
+        Plants plants = plantsList.get(position);
+            Picasso.get().load(plants.getPlantImage()).into(holder.imgPlants);
+            holder.tvName.setText(""+plants.getCommonName());
+            holder.tvFamily.setText("" + plants.getFamily());
+
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        if(plantsList != null){
+            return showShimmer ? 1 : plantsList.size();
+        }
+        return 0;
+    }
+    public void setShowShimmer(boolean showShimmer) {
+        this.showShimmer = showShimmer;
+        notifyDataSetChanged();
+    }
+    public void setFilteredList(List<Plants> filteredList){
+        this.plantsList = filteredList;
+        notifyDataSetChanged();
+    }
+    public void updateData(List<Plants> newPlantList) {
+        this.plantsList = newPlantList;
+        notifyDataSetChanged();
+    }
+
+    public class PlantViewHolder extends RecyclerView.ViewHolder{
+        private TextView tvName, tvFamily;
+        private CircleImageView imgPlants;
+        private ShimmerFrameLayout shimmerLayout;
+        public PlantViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            tvName = itemView.findViewById(R.id.tv_plant_name_search);
+            tvFamily = itemView.findViewById(R.id.tv_plant_family_search);
+            shimmerLayout = itemView.findViewById(R.id.shimmer_layout_plant_search);
+            imgPlants = itemView.findViewById(R.id.img_plant_search);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(recyclerViewInterface != null){
+                        int pos = getAdapterPosition();
+                        if (pos != RecyclerView.NO_POSITION && pos < plantsList.size() && !showShimmer){
+                            recyclerViewInterface.onItemClick(recyclerViewId,pos);
+                            Plants plant = plantsList.get(pos);
+                            Bundle bundle = new Bundle();
+
+                                bundle.putInt("plant_id",plant.getPlantID());
+                                bundle.putString("plant_scientificName",plant.getScientificName());
+                                bundle.putString("plant_commonName",plant.getCommonName());
+                                bundle.putString("plant_family", plant.getFamily());
+                                bundle.putString("plant_genus", plant.getGenus());
+                                bundle.putString("plant_spieces", plant.getSpecies());
+                                bundle.putString("plant_description", plant.getDescription());
+                                bundle.putString("plant_growth_rate", plant.getGrowthRate());
+
+                                bundle.putString("plant_light", plant.getLightRequirements());
+                                bundle.putString("plant_water", plant.getWaterRequirements());
+                                bundle.putString("plant_hard", plant.getCareRequirements());
+                                bundle.putString("plant_soil", plant.getSoilType());
+                                bundle.putString("plant_ph", plant.getPHRange());
+                                bundle.putString("plant_temperature", plant.getTemperatureRange());
+                                bundle.putString("plant_bloom", plant.getBloomtime());
+                                bundle.putString("plant_propagation", plant.getPropagation());
+                                bundle.putString("plant_size", plant.getSize());
+                                bundle.putString("plant_img", plant.getPlantImage());
+
+                                Intent intent = new Intent(context, DetailActivity.class);
+                                intent.putExtra("plant_infor", bundle);
+                                context.startActivity(intent);
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+}
--- app/src/main/java/com/example/plant_library/Fragment/SearchFragment.java
+++ app/src/main/java/com/example/plant_library/Fragment/SearchFragment.java
@@ -2,6 +2,7 @@ package com.example.plant_library.Fragment;
 
 import static android.service.controls.ControlsProviderService.TAG;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -24,11 +25,14 @@ import com.example.plant_library.Activity.GenreActivity;
 import com.example.plant_library.Activity.IndexActivity;
 import com.example.plant_library.Adapter.GenreAdapter;
 import com.example.plant_library.Adapter.PlantCategoryAdapter;
+import com.example.plant_library.Adapter.PlantsAdapter;
+import com.example.plant_library.Adapter.PlantsSearchAdapter;
 import com.example.plant_library.FragmentHelper;
 import com.example.plant_library.Interface.FragmentHandler;
 import com.example.plant_library.Interface.RecyclerViewInterface;
 import com.example.plant_library.Object.Genre;
 import com.example.plant_library.Object.PlantCategory;
+import com.example.plant_library.Object.Plants;
 import com.example.plant_library.R;
 import com.google.firebase.Firebase;
 import com.google.firebase.database.DataSnapshot;
@@ -38,6 +42,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 
@@ -45,13 +50,15 @@ import java.util.Locale;
 public class SearchFragment extends Fragment implements RecyclerViewInterface {
     private static final String TAG = "SearchFragment";
     GenreAdapter genreAdapter;
-    RecyclerView recyclerView;
+    RecyclerView recyclerView, recyclerViewSearch;
     PlantCategoryAdapter plantCategoryAdapter;
+    PlantsSearchAdapter plantsAdapter;
     SearchView searchView;
     View mView;
     private FragmentHandler fragmentHandler;
     private List<PlantCategory> plantCategoryList;
     private List<Genre> genreList;
+    private List<Plants> plantsList, plantsListFull;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -59,14 +66,88 @@ public class SearchFragment extends Fragment implements RecyclerViewInterface {
         mView = inflater.inflate(R.layout.fragment_search, container, false);
         fragmentHandler = new FragmentHelper(getActivity().getSupportFragmentManager(), R.id.frame_search);
         searchView = mView.findViewById(R.id.sv_search_view);
+        recyclerViewSearch = mView.findViewById(R.id.rcv_search);
         setGenreAdapter();
         setPlantCategoryAdapter();
+        setPlantsAdapter();
+        searchView();
         return mView;
     }
 
     private void searchView(){
-        searchView.setOn
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                filter(newText);
+                recyclerViewSearch.setVisibility(View.VISIBLE);
+                plantsAdapter.notifyDataSetChanged();
+                if(newText.isEmpty()){
+                    recyclerViewSearch.setVisibility(View.GONE);
+                }
+                return false;
+            }
+        });
     }
+    public void filter(String text) {
+       List<Plants> plantFilter = new ArrayList<>();
+        for (Plants plants : plantsList){
+            if(plants.getCommonName().toLowerCase().contains(text.toLowerCase())){
+                plantFilter.add(plants);
+            }
+        }
+        if (plantFilter.isEmpty()){
+
+        }else {
+            plantsAdapter.setFilteredList(plantFilter);
+        }
+    }
+    private void setPlantsAdapter(){
+        recyclerView = mView.findViewById(R.id.rcv_search);
+        plantsList = new ArrayList<>();
+        plantsAdapter = new PlantsSearchAdapter(plantsList,this, getContext(), R.id.rcv_search);
+
+        LinearLayoutManager manager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL,false);
+        recyclerView.setLayoutManager(manager);
+
+        recyclerView.setAdapter(plantsAdapter);
+        getListInterestPlants();
+
+
+    }
+    private void getListInterestPlants() {
+        DatabaseReference plantsRef = FirebaseDatabase.getInstance().getReference("Plants");
+        plantsRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Plants plant = snapshot.getValue(Plants.class);
+                    if (plant != null) {
+                        plantsList.add(plant);
+                    }
+                }
+                plantsAdapter.notifyDataSetChanged();
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        plantsAdapter.setShowShimmer(false);
+                    }
+                }, 3000);
+                Log.d(ContentValues.TAG, "Number of plants loaded: " + plantsList.size());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e(ContentValues.TAG, "Error: " + error.getMessage());
+            }
+        });
+    }
+
     private void setGenreAdapter() {
         recyclerView = mView.findViewById(R.id.rcv_genre);
         genreList = new ArrayList<>();
